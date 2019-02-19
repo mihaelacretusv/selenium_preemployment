5260d91140bc49c91ba3c5a9f634121b57172ab3
claudiu.iacob <claudiu.iacob@P5495.ro.softvision.global>
2019-02-19 18:01:50 +0200

    SeleniumAutomation-good one

---
+++ claudiu/src/main/java/util/Constants.java
@@ -0,0 +1,6 @@
+package util;
+public class Constants {
+    public static final String BASE_URL="http://automationpractice.com/index.php";
+    public static final String USERNAME="mihaela.cretu@softvision.ro";
+    public static final String PASSWORD="Test1234";
+}
