2796bbbf4083676e02d9e2c18955a4f1f19cffd0
Alex Caciur <alexandru.caciur@P5808.ro.softvision.global>
2019-02-19 09:02:53 +0200

    webdriver methods

---
+++ caciuralex/src/main/java/Main.java
@@ -0,0 +1,53 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) {
+        String pathToDriver = "D:\\chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", pathToDriver);
+
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("http://automationpractice.com/index.php");
+
+        WebElement element = webDriver.findElement(By.name("search_query"));
+        List<WebElement> products = webDriver.findElements(By.className("product-image-container"));
+
+
+        //get attribute
+        System.out.println("Get element attribute");
+        System.out.println(element.getAttribute("placeholder"));
+        webDriver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
+
+        //type text in text entry element
+        System.out.println("Type text in text entry element");
+        element.sendKeys("goooogle");
+        webDriver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
+
+        //insert text
+        System.out.println("Get text");
+        System.out.println(element.getText());
+        webDriver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
+
+        //clear
+        System.out.println("Clear text from entry element");
+        element.clear();
+        webDriver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
+
+        WebElement product = webDriver.findElement(By.className("product-container"));
+
+        List<WebElement> productName = product.findElements(By.className("product-name"));
+
+        System.out.println(productName.size());
+
+        List<WebElement> productPrice = product.findElements(By.className("product-price"));
+
+        System.out.println(productPrice.size());
+
+        webDriver.close();
+    }
+}
